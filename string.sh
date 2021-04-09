9cda37071709792f06ccbf12e77414f16f9be48d
king-cobra-user <78993442+king-cobra-user@users.noreply.github.com>
2021-04-09 09:57:25 +0530

    Add files via upload

---
+++ string.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#   Copyright (C) 2021 KINGCOBRAOP
+#
+#   Poject EAGLEBOT is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+                      © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃℄1�7
+           ╔═╗╔═╦═══╦═══╦══╦═══╦══╗╔═══╦════╗
+           ║║╚╝║║╔═╗║╔══╩╣╠╣╔═╗║╔╗║║╔═╗║╔╗╔╗║
+           ║╔╗╔╗║║─║║╚══╗║║║║┄1�7║║╚╝╚╣║─║╠╝║║╚╄1�7
+           ║║║║║║╚═╝║╔══╝║║║╚═╝║╔═╗║║┄1�7║║┄1�7║║
+           ║║║║║║╔═╗║║─┄1�7╔╣╠╣╔═╗║╚═╝║╚═╝║┄1�7║║
+           ╚╝╚╝╚╩╝─╚╩╝─┄1�7╚══╩╝─╚╩═══╩═══╝┄1�7╚╝
+                     © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_𝙼𝙰𝙵𝙸𝙰𝙱𝙾𝚃℄1�7                       
+                         @EAGLE_USERBOT
+"""
+
+python3 string_session.py
